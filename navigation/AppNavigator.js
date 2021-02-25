9dd02a939472dae4c13ec596732d7aa282abab6e
MacKenzie Joyal <mackenzie.joyal@gmail.com>
2021-02-24 16:53:21 -0800

    add main navigator (tab navigator with nested stack)

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,12 +2,16 @@ import React from 'react'
 
 import { NavigationContainer } from '@react-navigation/native'; 
 import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeScreen from '../screens/HomeScreen'; 
 import LoginScreen from '../screens/LoginScreen'; 
 import QueueScreen from '../screens/QueueScreen'; 
+import FavouritesScreen from '../screens/FavouritesScreen'; 
+import SongLibraryScreen from '../screens/SongLibraryScreen'; 
 
 const Stack = createStackNavigator(); 
+const Tab = createBottomTabNavigator(); 
 
 const StackNavigator = () => {
   return (
@@ -28,10 +32,29 @@ const StackNavigator = () => {
   )
 }; 
 
+const MainNavigator = () => {
+ return (
+    <Tab.Navigator>
+    <Tab.Screen 
+      name="Home"
+      component={StackNavigator}
+    />
+    <Tab.Screen 
+      name="Favourites"
+      component={FavouritesScreen}
+    />
+    <Tab.Screen 
+      name="Library"
+      component={SongLibraryScreen}
+    />
+  </Tab.Navigator>
+ )
+}; 
+
 const AppNavigator = () => {
   return (
     <NavigationContainer>
-      <StackNavigator/>
+      <MainNavigator/>
     </NavigationContainer>
   )
 }; 
